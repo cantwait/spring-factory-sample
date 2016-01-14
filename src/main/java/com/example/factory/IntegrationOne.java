c42807ac313737b6dc58f2671613a8170f1d4835
cantwait <cadenas.rafael@gmail.com>
2016-01-14 13:35:53 -0500

    testing injecting component into factory object

---
+++ src/main/java/com/example/factory/IntegrationOne.java
@@ -0,0 +1,12 @@
+package com.example.factory;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IntegrationOne {
+	
+	public void integrate(){
+		System.out.println("Integrating...!");
+	}
+
+}
--- src/main/java/com/example/factory/TwoType.java
+++ src/main/java/com/example/factory/TwoType.java
@@ -1,11 +1,15 @@
 package com.example.factory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 public class TwoType implements Factory{
 	
 	@Value("${prop.value}")
 	private String prop;
+	
+	@Autowired
+	IntegrationOne integration;
 
 	@Override
 	public void sayHello() {
@@ -14,6 +18,7 @@ public class TwoType implements Factory{
 
 	@Override
 	public String echo(String msg) {
+		integration.integrate();
 		System.out.println("printing property: " +prop + " from twotype");
 		return "Echoing from type two..." + msg;
 	}
